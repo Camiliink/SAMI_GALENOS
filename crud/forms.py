dcc06b9817e676d772b941a42e8119e7e7e67ffd
Benja-pag <benjaminp.aravenag@gmail.com>
2024-11-18 19:57:17 -0300

    commits 1
    1

--- crud/forms.py
+++ crud/forms.py
@@ -1,8 +1,11 @@
-from django import forms 
-from .models import Usuario
-
+# En forms.py
+from django import forms
+from .models import Usuario, TIPO_USUARIO_CHOICES  # Importa el listado de opciones
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
+        widgets = {
+            'tipo_usuario': forms.Select(choices=TIPO_USUARIO_CHOICES),  # Aquí usas las opciones importadas
+        }
--- crud/models.py
+++ crud/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Opciones de tipo de usuario
+TIPO_USUARIO_CHOICES = [
+    ('medico', 'Médico'),
+    ('paciente', 'Paciente'),
+    ('administrador', 'Administrador'),
+    ('cajera', 'Cajera'),
+    ('secretaria', 'Secretaria'),
+]
+
 class Usuario(models.Model):
-    code = models.AutoField(primary_key=True,verbose_name="codigo")
+    code = models.AutoField(primary_key=True, verbose_name="codigo")
     rut = models.CharField(max_length=12)
-    nombre_usuario = models.CharField(max_length=20,verbose_name="Nombre Usuario")
-    nombre_completo = models.CharField(max_length=50,verbose_name="Nombre Completo")
-    contrasenna = models.CharField(max_length=20,verbose_name="Contraseña")
-    tipo_usuario = models.CharField(max_length=20,verbose_name="Tipo Usuario")
+    nombre_usuario = models.CharField(max_length=20, verbose_name="Nombre Usuario")
+    nombre_completo = models.CharField(max_length=50, verbose_name="Nombre Completo")
+    correo = models.EmailField(max_length=100, blank=True, null=True)  # Campo de correo
+    contrasenna = models.CharField(max_length=20, verbose_name="Contraseña")
+    tipo_usuario = models.CharField(max_length=20, choices=TIPO_USUARIO_CHOICES, verbose_name="Tipo Usuario")
     
+    def save(self, *args, **kwargs):
+        # Genera el correo automáticamente a partir del nombre completo
+        if not self.correo:  # Si no se proporciona correo, lo genera
+            self.correo = f"{self.nombre_completo.replace(' ', '').lower()}@gmail.com"
+        super(Usuario, self).save(*args, **kwargs)
+
     def __str__(self):
         fila = "Nombre: " + self.nombre_completo + " - " + "Nombre Usuario: " + self.nombre_usuario + " - " + "Tipo Usuario: " + self.tipo_usuario
         return fila
-    
