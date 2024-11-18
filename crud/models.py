834013ee05360892da6b8eb179307fdd41967444
Camiliink <cam.jopia@duocuc.cl>
2024-11-17 23:32:03 -0300

    add rut to model

--- crud/models.py
+++ crud/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
     code = models.AutoField(primary_key=True,verbose_name="codigo")
+    rut = models.CharField(max_length=12)
     nombre_usuario = models.CharField(max_length=20,verbose_name="Nombre Usuario")
     nombre_completo = models.CharField(max_length=50,verbose_name="Nombre Completo")
     contrasenna = models.CharField(max_length=20,verbose_name="Contraseña")
