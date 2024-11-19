b4a0b52053e8e903d2cd9939b8670a37a75a1939
Camiliink <cam.jopia@duocuc.cl>
2024-11-19 01:29:43 -0300

    LOGIN
    MAIN

--- crud/urls.py
+++ crud/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),  # Página principal
+    path('', views.login, name='login'),
+    path('inicio/', views.inicio, name='inicio'),  # Página principal
     path('nosotros', views.nosotros, name='nosotros'),  # Página de "Nosotros"
     path('usuarios', views.usuarios, name='usuarios'),  # Lista de usuarios
     path('hora', views.hora, name='hora'),  # Vista de hora
     path('usuarios/crear/', views.crear, name='crear'),  # Crear usuario
     path('usuarios/editar/<int:code>/', views.editar, name='editar'),  # Editar usuario específico
     path('usuarios/eliminar/<int:code>/', views.eliminar, name='eliminar'),  # Eliminar usuario específico
-     path('login/', views.login, name='login'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
